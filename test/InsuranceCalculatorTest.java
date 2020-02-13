f30c74dd6ea4b4472e2b789a518017a5fa4fdfd3
tmskinner <tmskinner@bsu.edu>
2020-02-13 11:23:46 -0500

    Creating and running test﻿

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -4,4 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceCalculatorTest {
 
+    public void low() {
+        assertEquals(1825, insuranceFor(5000), 0.01);
+    }
+
+    public void medium() {
+        assertEquals(38600, insuranceFor(25000), 0.01);
+    }
+
+    public void high() {
+        assertEquals(78500, insuranceFor(50000), 0.01);
+    }
+
+    public void veryHigh() {
+        assertEquals(106400, insuranceFor(100_000), 0.01);
+    }
+
+    private double insuranceFor(double income) {
+        return new InsuranceCalculator().calculateInsurance(income);
+    }
+
 }
